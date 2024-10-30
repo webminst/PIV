c080bfc019bcf7c414f33657498494b9351ee452
Giovanni Moreira Guimarães <46898700+webminst@users.noreply.github.com>
2024-10-30 13:14:59 -0300

    Add files via upload
    Novas adições

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 // script.js
 // Adicione o JavaScript para funções interativas, como menus suspensos, animações ou formulários
+
+const librasButton = document.getElementById('librasButton');
+
+librasButton.addEventListener('click', () => {
+  window.location.href = 'https://www.seu-servico-de-libras.com'; // Substitua pelo link do seu serviço
+});
+
+// Ação para o botão "Topo"
+document.getElementById('top-btn').addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
